631345e4c962ebdf478187a441f6f71cd1ad2272
Patrick Moye <Bigbook@Mac.fritz.box>
2026-07-25 03:11:07 +0200

    Prepare GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: isGithubPages ? "export" : undefined,
+  basePath: isGithubPages ? "/zeitmanagement-animationen" : undefined,
+  assetPrefix: isGithubPages ? "/zeitmanagement-animationen/" : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
